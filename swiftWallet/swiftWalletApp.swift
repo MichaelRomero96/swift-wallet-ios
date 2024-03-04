2fb0b715e258e922f57ee30cd2bc25d342d31029
MichaelRomeroSG <michael@socialgoodsoftware.com>
2024-03-03 22:12:25 -0500

    Initial commit

---
+++ swiftWallet/swiftWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftWalletApp.swift
+//  swiftWallet
+//
+//  Created by Michael Romero on 3/03/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
